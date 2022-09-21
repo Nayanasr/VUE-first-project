a56ce125b02ec9d96abde08a100a0484d21f67f5
Nayana Ramesh <107012@tfcorp.com>
2022-09-21 20:07:30 +0530

    first project of vue

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <h1>Hello World</h1>
+
+  <!-- rendering MyComponent -->
+  <!-- /<my-component greeting="Good Morning" :UserName="UserName" :rollNum="130" /> -->
+  <!-- :value="value" -->
+  <!-- we give v-bind(:) to use dynamic properties UserName -->
+
+  <!-- sending function to child component use v-on or @ -->
+  <child-component @getDataFromChild="getDataFromChild" />
+
+  <h3>Toy Data from child : {{ kidsToyData }}</h3>
+</template>
+
+<script>
+import ChildComponent from "./components/ChildComponent.vue";
+// import MyComponent from "./components/MyComponent.vue";
+export default {
+  name: "App",
+  components: {
+    // MyComponent,
+    ChildComponent,
+  },
+  //data
+  data() {
+    return {
+      UserName: "Raju Rastogi",
+      value: true,
+      kidsToyData: "",
+    };
+  },
+  methods: {
+    //chocholate function
+    //1.in parent component a function and pass it to child component
+    //2.call that function in child component and pass the data inside that function
+
+    getDataFromChild(childData) {
+      console.log("chocholate function executed", childData);
+      this.kidsToyData = childData;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
